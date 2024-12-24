c08b10f6a89700a5839a3da9b5766af0f24f980a
Ammar Omer <amar@livestreamlines.com>
2024-12-24 13:51:06 +0400

    resetemail66

--- controllers/authController.js
+++ controllers/authController.js
@@ -96,7 +96,7 @@ function sendResetPasswordLink(req, res) {
   <div style="max-width: 600px; margin: auto; font-family: Arial, sans-serif; border: 1px solid #ddd; border-radius: 8px; overflow: hidden;">
     <!-- Header Section with Logo -->
     <div style="background: #710707; padding: 20px; text-align: center;">
-      <img src="http://5.9.85.250:4200/assets/lsl.svg" alt="Company Logo" style="max-height: 50px;">
+      <img src="http://5.9.85.250:5000/logos/developer/logo.png" alt="Company Logo" style="max-height: 50px;">
     </div>
 
     <!-- Main Content Section -->

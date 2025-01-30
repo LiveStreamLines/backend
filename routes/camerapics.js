481433cca4e04271593df7b2822bde56a0c59ce2
Ammar Omer <amar@livestreamlines.com>
2025-01-30 12:51:58 +0400

    emaarnoauth

--- routes/camerapics.js
+++ routes/camerapics.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const cameraPicsController = require('../controllers/cameraPicsController');
 const authMiddleware = require('../controllers/authMiddleware');
 
+
+router.get('/emaar/:developerId/:projectId/:cameraId', cameraPicsController.getEmaarPics);
 router.use(authMiddleware);
 // Define the route to get camera pictures by developer, project, and camera ID, with an optional date filter
 router.post('/:developerId/:projectId/:cameraId/pictures/', cameraPicsController.getCameraPictures);
 router.get('/preview/:developerId/:projectId/:cameraId/', cameraPicsController.getCameraPreview);
 router.get('/preview-video/:developerId/:projectId/:cameraId/', cameraPicsController.generateWeeklyVideo);
-router.get('/emaar/:developerId/:projectId/:cameraId', cameraPicsController.getEmaarPics);
 
 module.exports = router;

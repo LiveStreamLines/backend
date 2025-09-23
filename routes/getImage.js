b748911dda137377f12188c6d58659a103bf746d
Ammar Omer <amar@livestreamlines.com>
2025-09-23 11:31:36 +0400

    seeb3

--- routes/getImage.js
+++ routes/getImage.js
@@ -4,7 +4,7 @@ const getImageController = require('../controllers/getImageController');
 const authMiddleware = require('../controllers/authMiddleware');
 
 // Apply authentication middleware only to this route
-router.use(authMiddleware.authMiddleware);
+router.use(authMiddleware);
 
 // Route to get images by date range with authentication required
 router.post('/:projectId/:cameraId/', getImageController.getImagesByDateRange);
